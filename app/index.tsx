3c6eb30398c3c2bae7b2ce0d67ac9d51fc93832f
dylan <drose138@gmail.com>
2025-08-28 18:22:29 -0400

    added create account button

--- app/index.tsx
+++ app/index.tsx
@@ -2,45 +2,62 @@ import { Button, ImageBackground, Keyboard, KeyboardAvoidingView, Platform, Styl
 
 export default function Index() {
     return (
-<TouchableWithoutFeedback onPress={Keyboard.dismiss}> 
-  {/* Dismiss keyboard when clicking outside of input fields anywhere on background including header */}
-        <ImageBackground style={styles.kite} source={require('../assets/images/kiteLogin.jpg')} resizeMode="cover">
-             
-             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.headerContainer}>
-                    <Text style={styles.header}>Kite or Knot</Text>
-                </View>
-              </TouchableWithoutFeedback>
-            
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            {/* Dismiss keyboard when clicking outside of input fields anywhere on background including header */}
+            <ImageBackground style={styles.kite} source={require('../assets/images/kiteLogin.jpg')} resizeMode="cover">
 
-            <KeyboardAvoidingView  
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
-                style={styles.container}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.header}>Kite or Knot</Text>
+                    </View>
+                </TouchableWithoutFeedback>
 
-                
+
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={styles.container}>
+
+
                     <View style={styles.inner}>
 
-                        <TextInput placeholder="Username" style={styles.textInput}></TextInput> 
-                      
+                        <TextInput placeholder="Username" style={styles.textInput}></TextInput>
+
                         <TextInput placeholder="Password" style={styles.textInput}></TextInput>
-                      
-                        <View style={styles.btnContainer}>
-                            <Button title="Login" onPress={() => null} />
+
+                        <View style={styles.loginbtnContainer} >
+                            <Button 
+                                title="Login" 
+                                onPress={() => null}   
+                                //color="#027600ff"
+                            />
                         </View>
-                    
+
+                        <View >
+                            <Text style={styles.partition}> or </Text>
+                        </View>
+                        
+
+                        <View style={styles.signupbtnContainer} >
+                            <Button 
+                                title="Create Account" 
+                                onPress={() => null}   
+                                color="#027600ff"
+                            />
+                        </View>
+
                     </View>
 
-                
-              </KeyboardAvoidingView>
-            
-        </ImageBackground>
+
+                </KeyboardAvoidingView>
+
+            </ImageBackground>
         </TouchableWithoutFeedback>
     );
 
 }
 
 const styles = StyleSheet.create({
-    
+
     container: {
         flex: 1,
     },
@@ -68,49 +85,80 @@ const styles = StyleSheet.create({
 
     textInput: {
         ...Platform.select({
-        ios: {
-          // iOS specific styles
-          borderWidth: 1,
-            borderRadius: 10,
-            height: 35,
-            width: 250,
-            marginTop: 10,
-            backgroundColor: 'white',
-        },
-        android: {
-          // Android specific styles
-          borderWidth: 1,
-            borderRadius: 10,
-            width: 250,
-            marginTop: 10,
-            backgroundColor: 'white',
-        },
-      }),
+            ios: {
+                // iOS specific styles
+                borderWidth: 1,
+                borderRadius: 10,
+                height: 35,
+                width: 250,
+                marginTop: 10,
+                backgroundColor: 'white',
+            },
+            android: {
+                // Android specific styles
+                borderWidth: 1,
+                borderRadius: 10,
+                width: 250,
+                marginTop: 10,
+                backgroundColor: 'white',
+            },
+        }),
     },
 
 
-    btnContainer: {
+    loginbtnContainer: {
         ...Platform.select({
-          ios: {
-            // iOS specific styles
-              borderRadius: 10,
-              alignSelf: 'center',
-              marginTop: 10,
-              width: 240,
-              backgroundColor: '#cececeff',
-          },
-          android: {
-            // Android specific styles
-              borderRadius: 10,
-              alignSelf: 'center',
-              marginTop: 10,
-              width: 240,
-          },
+            ios: {
+                // iOS specific styles
+                borderRadius: 10,
+                alignSelf: 'center',
+                marginTop: 10,
+                width: 240,
+                backgroundColor: '#e0e0e0ff',
+                
+            },
+            android: {
+                // Android specific styles
+                borderRadius: 10,
+                alignSelf: 'center',
+                marginTop: 10,
+                width: 240,
+            },
+
+        }),
+    },
+
+
+    signupbtnContainer: {
+        borderRadius: 10,
+        alignSelf: 'center',
+        marginTop: 10,
+        width: 220,
+
+        ...Platform.select({
+            ios: {
+                // iOS specific styles
+                
+                backgroundColor: '#e0e0e0ff',
+                
+            },
+            android: {
+                // Android specific styles
+            
+            },
+            
         }),
     },
 
     kite: {
         flex: 1,
-    }
+    },
+
+    partition: {
+        // marginTop: 5,
+        // marginBottom: 5,
+        fontSize: 30,
+        color: '#444444ff',
+    },
 })
 
