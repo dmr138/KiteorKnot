e7c8b3096d2900c37e083de08526f6107d555635
dylan <drose138@gmail.com>
2025-10-01 16:32:39 -0400

    cleaned up getWeather

--- utils/getWeather.ts
+++ utils/getWeather.ts
@@ -1,27 +1,11 @@
+import { supabase } from '@/utils/supabase';
 export  default async function getWeather(token: string): Promise<string> {
     if (!token) {return `Unauthorized - valid token required`;}          
-    const response = await fetch('https://weatherapi.turbophil.xyz/', {
+    const { data, error } = await supabase.functions.invoke('smooth-api', {
     headers: {
-        Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     },
-    });
-
-    if (response.status === 401) {
-    return `Unauthorized - valid token required`;
-    }
-    const data = await response.json();
+  });
+    if (error) return `Error: ${error.message}`;
     return JSON.stringify(data, null, 2);
-    };
-
-
-//old version for backup
-// const { data, error } = await supabase.functions.invoke('smooth-api', { 
-//   headers: { 
-//     Authorization: Bearer ${ session?.access_token }, 
-//     }, 
-//   }); 
-  
-//     if (error) { 
-//       console.error(error); setResult(Error: ${ error.message });
-//     } 
-//     else { setResult(JSON.stringify(data, null, 2)); } }}
+    }
